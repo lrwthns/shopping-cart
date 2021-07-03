d08041c0e11caa4c19ce4f3ba6e675b12fed2f6d
lrwthns <lrwthns@gmail.com>
2021-07-04 00:08:44 +0700

    Edit stylesheet

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -5,10 +5,12 @@ const ProductDetails = (props) => {
   const { products } = props;
   return (
     <div className="ProductDetails">
-      <img src={products[id].img1} alt=""/>
+      <img src={products[id].img1} alt={products[id].name}/>
       {/* <img src={products[id].img2} alt=""/> */}
-      <div className="productName">{products[id].name}</div>
-      <div className="productPrice">{products[id].price + ' USD'}</div>
+      <div className="details">
+        <div className="productName">{products[id].name}</div>
+        <div className="productPrice">{products[id].price + ' USD'}</div>
+      </div>
     </div>
   );
 }
